70bfe01337cfae5d4eecee773c73f28fd69ef2bc
Maartz <maartz@protonmail.com>
2019-04-25 15:53:37 +0200

    feat: add Calcul view and Discount Rate component + routing + import Vuesax globally

---
+++ src/components/DiscountRate.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1>Discount Rate</h1>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+
+    @Component
+    export default class DiscountRate extends Vue {}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// @ts-ignore
+import Vuesax from 'vuesax';
 import './registerServiceWorker';
 
 Vue.config.productionTip = false;
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+Vue.use(Vuesax);
 
 new Vue({
   router,

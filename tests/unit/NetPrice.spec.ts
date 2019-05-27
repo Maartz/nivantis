02d4ea9a06840640d89b79c24bc22b8179720b55
Romain Dejoux <romain.dejoux@laposte.net>
2019-05-27 08:40:50 +0200

    feat: implement a netPrice component

---
+++ tests/unit/NetPrice.spec.ts
@@ -0,0 +1,14 @@
+import { shallowMount } from '@vue/test-utils';
+import NetPrice from '@/components/NetPrice.vue';
+
+describe('NetPrice.vue', () => {
+    it('Component should calculate prices', async () => {
+        const wrapper = shallowMount(NetPrice, {});
+
+        const component = await (wrapper.vm as any);
+
+        expect(component.getNetPrice(100, 0.5)).toEqual(50);
+        expect(component.getRawPrice(50, 0.5)).toEqual(100);
+        expect(component.getDiscountRate(50, 100)).toEqual(0.5);
+    });
+});

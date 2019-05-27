cea3ac9a158b9e1169583dc7acf4914ea60239aa
Romain Dejoux <romain.dejoux@laposte.net>
2019-05-27 08:41:12 +0200

    Update: display the netPrice component on calcul view

--- src/views/Calcul.vue
+++ src/views/Calcul.vue
@@ -2,6 +2,7 @@
     <div>
         <h1>Calcul</h1>
         <DiscountRate/>
+        <NetPrice/>
     </div>
 </template>
 
@@ -9,10 +10,12 @@
 <script lang="ts">
     import { Component, Vue } from 'vue-property-decorator';
     import DiscountRate from '@/components/DiscountRate.vue'; // @ is an alias to /src
+    import NetPrice from '@/components/NetPrice.vue';
 
     @Component({
         components: {
             DiscountRate,
+            NetPrice
         },
     })
     export default class Calcul extends Vue {}

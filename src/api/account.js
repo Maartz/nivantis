c2598dd1aeaedf3582e3d2b72c0bf2a6cd885c91
Maartz <maartz@protonmail.com>
2019-06-17 11:52:44 +0200

    Update mock to fit good architecture practice

---
+++ src/api/account.js
@@ -0,0 +1,16 @@
+export const account = [
+  {
+    "type" : "DMO",
+    "firstName" : "Albert",
+    "lastName" : "1er de Belgique",
+    "stores" : [
+      {
+        "name" : "bla",
+        "SIRET": "1234567",
+        "address" : "rue des putes",
+        "lat": 12.5,
+        "long": 321412.23
+      }
+    ]
+  },
+];
